d423380b4a6ddeaa942e03b5e1b64ce3eb08f6d9
cdirks4 <cdirks4@me.com>
2022-09-25 07:45:22 -0400

    morning-test

--- index.mjs
+++ index.mjs
@@ -1,100 +1,108 @@
 import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs'
-
+import * as backend from './build/index.main.mjs';
 
 const stdlib = loadStdlib();
-const startingBalance = stdlib.parseCurrency(1000)
+const startingBalance = stdlib.parseCurrency(1000);
 
 const accCreator = await stdlib.newTestAccount(startingBalance);
 
-const tokA = await stdlib.launchToken(accCreator, "tokenA", "Atok");  
-const tokB = await stdlib.launchToken(accCreator, "gil", "Btok");
+const tokA = await stdlib.launchToken(accCreator, 'tokenA', 'Atok');
+const tokB = await stdlib.launchToken(accCreator, 'gil', 'Btok');
 
+const swappers = [];
 
+const startSwappers = async (lpTok) => {
+	const runSwapper = async (swapper) => {
+		console.log('swappers running');
+		// creating test account for api
+		const acc = await stdlib.newTestAccount(startingBalance);
+		acc.setDebugLabel(swapper);
 
-const swappers = []
+		// opting in to tokens
+		await acc.tokenAccept(tokA.id);
+		await acc.tokenAccept(tokB.id);
+		// paying account tokens
+		await tokA.mint(acc, 1000);
+		await tokB.mint(acc, 1000);
+		swappers.push(acc);
+		// attaching api users to deployers contract
+		const ctc = acc.contract(backend, ctcDeployer.getInfo());
 
-const startSwappers = async (lpTok) =>{
-    const runSwapper = async (swapper) =>{
-            console.log('swappers running')
-            // creating test account for api
-            const acc = await stdlib.newTestAccount(startingBalance);
-            acc.setDebugLabel(swapper)
+		console.log(
+			`${swapper} current bal of tokA is ${await acc.balanceOf(tokA.id)}`
+		);
+		console.log(
+			`${swapper} current bal of tokB is ${await acc.balanceOf(tokB.id)}`
+		);
 
-            // opting in to tokens
-            await acc.tokenAccept(tokA.id)
-            await acc.tokenAccept(tokB.id)
-            // paying account tokens
-            await tokA.mint(acc,1000)
-            await tokB.mint(acc,1000)
-            swappers.push(acc)
-            // attaching api users to deployers contract
-    const ctc = acc.contract(backend, ctcDeployer.getInfo());
+		try {
+			// depositing tokens to liquidity pool
+			const res = await ctc.apis.Swapper.deposit({
+				firstTok: tokA,
+				secondTok: tokB,
+				firstSupply: 10,
+				secondSupply: 20,
+			});
+			console.log(res);
+			console.log(
+				`${swapper} post bal of tokA is ${await acc.balanceOf(tokA.id)}`
+			);
+			console.log(
+				`${swapper} post bal of tokB is ${await acc.balanceOf(tokB.id)}`
+			);
+		} catch (err) {
+			console.log('Error in deposit func');
+		}
 
-            console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA.id)}`)
-            console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB.id)}`)
+		//         const tokenSupplyObj = Struct([
+		//         ['firstTok', Token],
+		//         ['secondTok', Token],
+		//         ['secondSupply', UInt],
+		//         ['firstSupply', UInt]
+		// ]
+		//     )
+		// try {
+		//     // withdrawing tokens from liquidity pool
+		//     await ctc.apis.Swapper.withdraw([{tok:lpTokId,supply:20}])
+		//     console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA)}`)
+		//     console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB)}`)
 
-        try {
-            // depositing tokens to liquidity pool
-            const res = await ctc.apis.Swapper.deposit({firstTok: tokA, secondTok: tokB, firstSupply:10, secondSupply:20})
-            console.log(res)
-            console.log(`${swapper} post bal of tokA is ${await acc.balanceOf(tokA.id)}`)
-            console.log(`${swapper} post bal of tokB is ${await acc.balanceOf(tokB.id)}`)
+		// } catch (err) {
+		//     console.log('Error in withdraw func')
+		// }
+		// try {
+		//     // swapping tokenA for token B
+		//     await ctc.apis.Swapper.swapAtoB([{tok: tokA, supply:10}])
+		//     console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA)}`)
+		//     console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB)}`)
 
-            
-        } catch (err) {
-            console.log('Error in deposit func')
-        }
+		// } catch (err) {
+		//     console.log('Error in AtoB func')
+		// }
+		// try {
+		//     // swapping token B to token A
+		//     await ctc.apis.Swapper.swapBtoA([{tok:tokB,supply:20}])
+		//     console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA)}`)
+		//     console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB)}`)
 
+		// } catch (err) {
+		//     console.log('Error in BtoA func')
+		// }
+	};
+	runSwapper('Alice');
+	runSwapper('Bob');
+	runSwapper('Charlie');
+};
 
-//         const tokenSupplyObj = Struct([
-//         ['firstTok', Token],
-//         ['secondTok', Token],
-//         ['secondSupply', UInt],
-//         ['firstSupply', UInt]
-// ]
-//     )
-        // try {
-        //     // withdrawing tokens from liquidity pool
-        //     await ctc.apis.Swapper.withdraw([{tok:lpTokId,supply:20}])
-        //     console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA)}`)
-        //     console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB)}`)
-
-            
-        // } catch (err) {
-        //     console.log('Error in withdraw func')
-        // }
-        // try {
-        //     // swapping tokenA for token B
-        //     await ctc.apis.Swapper.swapAtoB([{tok: tokA, supply:10}])
-        //     console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA)}`)
-        //     console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB)}`)
-
-            
-        // } catch (err) {
-        //     console.log('Error in AtoB func')
-        // }
-        // try {
-        //     // swapping token B to token A
-        //     await ctc.apis.Swapper.swapBtoA([{tok:tokB,supply:20}])
-        //     console.log(`${swapper} current bal of tokA is ${await acc.balanceOf(tokA)}`)
-        //     console.log(`${swapper} current bal of tokB is ${await acc.balanceOf(tokB)}`)
-
-            
-        // } catch (err) {
-        //     console.log('Error in BtoA func')
-        // }
-    }
-    runSwapper('Alice')
-    runSwapper('Bob')
-    runSwapper('Charlie')
-}
-
-const accDeployer = await stdlib.newTestAccount(startingBalance)
-const ctcDeployer = accDeployer.contract(backend)
-console.log(ctcDeployer)
-await ctcDeployer.participants.Deployer({
-    swapReady: (lpTokId) =>{
-        startSwappers(lpTokId)
-    }
-},backend)
+const accDeployer = await stdlib.newTestAccount(startingBalance);
+const ctcDeployer = accDeployer.contract(backend);
+console.log(ctcDeployer);
+await ctcDeployer.participants.Deployer(
+	{
+		getTokens: [tokA.id, tokB.id],
+		swapReady: (lpTokId) => {
+			startSwappers(lpTokId);
+		},
+	},
+	backend
+);
--- index.rsh
+++ index.rsh
@@ -1,68 +1,79 @@
-'reach 0.1'
+'reach 0.1';
 
+export const main = Reach.App(() => {
+	const Deployer = Participant('Deployer', {
+		getTokens: Array(Token, 2),
+		swapReady: Fun([Token], Null),
+	});
 
-const tokenSupplyObj = Struct([
-        ['firstTok', Token],
-        ['secondTok', Token],
-        ['secondSupply', UInt],
-        ['firstSupply', UInt]
-]
-    )
-const singleTokenSupplyObj = Struct([
-        ['firstTok', Token],
-        ['secondTok', Token],
-]
-    )
+	const supply = UInt.max;
 
+	const tokenSupplyObj = Struct([
+		['secondSupply', UInt],
+		['firstSupply', UInt],
+	]);
+	const SwapperApi = API('Swapper', {
+		deposit: Fun([tokenSupplyObj], Bytes(5)),
+		// withdraw: Fun([singleTokenSupplyObj],Null ),
+		// swapAtoB: Fun([singleTokenSupplyObj], Null),
+		// swapBtoA: Fun([singleTokenSupplyObj], Null)
+	});
+	init();
+	Deployer.only(() => {
+		const [tokAID, tokBID] = declassify(interact.getTokens);
+		assume(tokAID != tokBID, 'I assumed');
+	});
+	Deployer.publish(tokAID, tokBID);
 
-export const main = Reach.App(()=>{
-    const Deployer = Participant('Deployer', {
-        swapReady:Fun([Token],Null)
-        tokA; 
-    })
-    const SwapperApi = API('Swapper',{
-        deposit: Fun([tokenSupplyObj], Bytes(5)),
-        // withdraw: Fun([singleTokenSupplyObj],Null ),
-        // swapAtoB: Fun([singleTokenSupplyObj], Null),
-        // swapBtoA: Fun([singleTokenSupplyObj], Null)   
-    })
-  
-    init()
-    const supply = UInt.max
-  
-    Deployer.publish()
-    const lpTok = Token.new({
-        supply
-    })
-    Deployer.interact.swapReady(lpTok)
-    commit()
-    Deployer.publish()
-    const [LPtot,Abal,Bbal] = parallelReduce([ supply,0,0 ])
-      .paySpec([lpTok])
-      .invariant(supply>0)
-      .while(true)
-      .api(SwapperApi.deposit, (obj,alert)=>{
-          const tokenStruct = tokenSupplyObj.fromObject(obj)
-          const tokenObj = tokenSupplyObj.toObject(obj)
-          const Ain = tokenObj.firstSupply
-          const Bin = tokenObj.secondSupply
-          const Atok = tokenObj.firstTok
-          const Btok = tokenObj.secondTok
-        return [0,[[Ain,Atok],[Bin,Btok]],(alert)=>{
-            const AbalPrime = Abal + Ain
-            const BbalPrime = Bbal + Bin
-            const denominator = AbalPrime * BbalPrime
-            const z =  muldiv(Ain,Bin,denominator)
-            const LPout = LPtot == 0 ? sqrt(Ain* Bin) : z/100 * LPtot 
-            const LPtotPrime = LPtot + LPout
-          
-            alert(null)
-            return [LPtotPrime,AbalPrime,BbalPrime]
-        }]
-        
-        // check(Ain > 0, "tokAsupply insufficient")
-        // check(Bin > 0, "tokBsupply insufficient")
-      })
-      commit()
-      exit()
-})
+	const lpTok = Token.new({
+		supply,
+	});
+
+	const singleTokenSupplyObj = Struct([
+		['firstTok', Token],
+		['secondTok', Token],
+	]);
+
+	Deployer.interact.swapReady(lpTok);
+	commit();
+	Deployer.publish();
+	const [LPtot, Abal, Bbal] = parallelReduce([supply, 0, 0])
+		.paySpec([tokAID, tokBID])
+		.invariant(true)
+		.while(true)
+		.api_(SwapperApi.deposit, (obj) => {
+			const tokenStruct = tokenSupplyObj.fromObject(obj);
+			const tokenObj = tokenSupplyObj.toObject(tokenStruct);
+			// check(tokenObj.firstSupply != 0);
+			// check(tokenObj.secondSupply != 0);
+			const Ain = tokenObj.firstSupply;
+			const Bin = tokenObj.secondSupply;
+			return [
+				[
+					0,
+					...[
+						[Ain, tokAID],
+						[Bin, tokBID],
+					],
+				],
+				(alert) => {
+					// check(Abal != 0);
+					// check(Bbal != 0);
+					const AbalPrime = Abal + Ain;
+					const BbalPrime = Bbal + Bin;
+					const denominator = AbalPrime * BbalPrime;
+					const z = muldiv(Ain, Bin, denominator);
+					const LPout = LPtot == 0 ? sqrt(Ain * Bin) : (z / 100) * LPtot;
+					const LPtotPrime = LPtot + LPout;
+					alert('pays!');
+					transfer(LPout, lpTok).to(this);
+					return [LPtotPrime, AbalPrime, BbalPrime];
+				},
+			];
+
+			// check(Ain > 0, "tokAsupply insufficient")
+			// check(Bin > 0, "tokBsupply insufficient")
+		});
+	commit();
+	exit();
+});
